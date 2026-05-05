2ccebb0bfd24b8b8e51583307fea4591a0437042
Kaio Melugo <129877895+Kaio330@users.noreply.github.com>
2026-05-05 13:05:20 -0300

    Add support for NotaFiscalAlgar in CriaObjetos

--- CriaObjetos.py
+++ CriaObjetos.py
@@ -2,11 +2,12 @@ import re
 from Nota_fiscal_Embratel import NotaFiscalEmbratel
 from Nota_fiscal_Vivo import NotaFiscalVivo
 from POO_Automacao import Estrategia, NotaFiscal
+from Nota_fiscal_Algar import NotaFiscalAlgar
 import pdfplumber as pdftool
 
 def CriaObjetos(pdf_texto):
     try:
-        regex_cnpj = r'(?:CNPJ Matriz\s*:\s*|C\d?NPJ:\s*)(\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2})'
+        regex_cnpj = r'(?:CNPJ Matriz\s*:\s*|C\d?NPJ:\s*|CNPJ\s*)(\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2})'
         match_cnpj = re.search(regex_cnpj, pdf_texto)
     
         cnpjOperadora = match_cnpj.group(1)
@@ -15,11 +16,18 @@ def CriaObjetos(pdf_texto):
             fatura = Estrategia(pdf_texto, NotaFiscal)
             fatura.definirNota(NotaFiscalVivo)
             return fatura.adicionarValores(pdf_texto)
+        
+        if cnpjOperadora == '05.872.814/0001-30' or '05.872.814/0007-25':
+            fatura = Estrategia(pdf_texto, NotaFiscal)
+            fatura.definirNota(NotaFiscalAlgar)
+            return fatura.adicionarValores(pdf_texto)
 
         if cnpjOperadora == '40.432.544/0001-47' or '02.667.694/0001-40':
             fatura = Estrategia(pdf_texto, NotaFiscal)
             fatura.definirNota(NotaFiscalEmbratel)
             return fatura.adicionarValores(pdf_texto)
+        
+
     except Exception as e:
         print(f"erro no processamento: {e}")
     return NotaFiscalEmbratel('', '', '', '', '', '', '','')
